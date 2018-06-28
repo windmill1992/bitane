cb3d09378cf34a9f77822b2b9f39b3369c2c3bfe
yup <1182657869@qq.com>
2018-06-28 14:55:56 +0800

    update

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,4 @@
 
-let base = process.env.NODE_ENV === 'development' ? '/api': "https://bitane.muzhao888.com";
+let base = process.env.NODE_ENV === 'development' ? '/api': "http://api.bitane.io/";
 
 export const baseUrl = base;
